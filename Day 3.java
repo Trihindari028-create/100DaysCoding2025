00da76487097c855667f0f54e686ee73d757f54f
Trihindari028-create <nndar028@gmail.com>
2025-09-10 18:11:51 +0800

    Day 3.java

---
+++ Day 3.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+     // Kelas utama bernama HelloWorld
+    public static void main(String[] args) {
+        System.out.println("Halo, indar!");
+        // Memunculkan pesan ke layar/ menampilkan
+    }
+}
