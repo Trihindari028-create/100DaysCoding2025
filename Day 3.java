68aa6b9984a026d400b1ff8bab11d7870be7aed4
Trihindari028-create <nndar028@gmail.com>
2025-09-10 22:22:12 +0800

    Day 3.java

--- Day 3.java
+++ Day 3.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Halo, indar!");
         // Memunculkan pesan ke layar/ menampilkan
+    /*
+ini juga komentar tapi untuk kalimat yang lebih panjang ,
+bisa untuk lebih dari satu baris 
+*/
     }
 }
