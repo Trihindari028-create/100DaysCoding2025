27c22a96a2f2f067b02c2c0f49e92b31b217017f
Trihindari028-create <nndar028@gmail.com>
2025-09-11 20:05:37 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Halo\nindar!");  // \n membuat baris baru
+        System.out.println("Nama\t: indar");  // \t menambahkan tab
+        System.out.println("Ini adalah tanda backslash: \\");  // \\ menampilkan \
+        System.out.println("indar berkata: \"Selamat malam!\"");  // \" menampilkan tanda kutip ganda
+        System.out.println("Karakter apostrof: \'");  // \' menampilkan tanda kutip tunggal
+    }
+}
