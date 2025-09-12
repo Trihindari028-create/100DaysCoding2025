d7d6e6c0f0cfa8de6c8737ee330fb60ae34daac4
Trihindari028-create <nndar028@gmail.com>
2025-09-12 19:20:23 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipe data byte (8-bit)
+        byte nilaiByte = 120;
+        System.out.println("Nilai byte: " + nilaiByte);
+
+        // Tipe data short (16-bit)
+        short nilaiShort = 30000;
+        System.out.println("Nilai short: " + nilaiShort);
+
+        // Tipe data int (32-bit)
+        int nilaiInt = 2000000000;
+        System.out.println("Nilai int: " + nilaiInt);
+
+        // Tipe data long (64-bit)
+        long nilaiLong = 9000000000000000000L;
+        System.out.println("Nilai long: " + nilaiLong);
+    }
+}
