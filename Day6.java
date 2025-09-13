7d5851220c2682ecdf63d9c52f4b3db11808ca34
Trihindari028-create <nndar028@gmail.com>
2025-09-13 20:10:25 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        float angkaFloat = 3.141f;
+        System.out.println("Nilai float: " + angkaFloat);
+        
+        double angkaDouble = 3.173;
+        System.out.println("Nilai double: " + angkaDouble);
+        
+        float hasilFloat = angkaFloat * 2;
+        double hasilDouble = angkaDouble * 2;
+      
+        System.out.println("Hasil float * 2: " + hasilFloat);
+        System.out.println("Hasil double * 2: " + hasilDouble);
+    }
+}
