0fca5085ef94bfc98f58a9f5f4f3d6534fa6c974
Trihindari028-create <nndar028@gmail.com>
2025-09-16 22:28:17 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int angka = 40;
+        String nama = "Dunia Boba";
+        
+        // Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+        System.out.println("Nilai awal nama: " + nama);
+        
+        // Update variabel
+        angka = 20;
+        nama = "Dunia Boba";
+        
+        // Menampilkan nilai variabel setelah update
+        System.out.println("Nilai angka setelah update: " + angka);
+        System.out.println("Nilai nama setelah update: " + nama);
+    }
+}
