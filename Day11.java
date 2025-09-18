f93dda18be970ab94cb0fff4d82bf9af143e50c8
Trihindari028-create <nndar028@gmail.com>
2025-09-18 17:18:54 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner bc = new Scanner(System.in);
+        int a = bc.nextInt();
+        System.out.println(a);
+    }
+}
