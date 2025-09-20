6c0d0708c71a589bb14dbfebdd8376717231214f
Trihindari028-create <nndar028@gmail.com>
2025-09-20 21:26:43 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("nama : tri");
+		System.out.print(" umur : 18 tahun");
+		System.out.printf(" nama : %s, umur : %d,ipk : %f", "tri" , 18 , 3.99);
+	}
+}
