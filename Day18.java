1379ef41689cea843c106ea2682c265bc5bac27d
Trihindari028-create <nndar028@gmail.com>
2025-09-25 19:39:33 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,11 @@
+public class Main {
+	
+	public static void main(String[] args) {
+	        
+        int nilaiInt = 100;
+        long nilaiLong = 200L;
+        long hasil = nilaiInt + nilaiLong;
+        System.out.println("Hasil int + long: " + hasil);
+	    
+	}
+}
