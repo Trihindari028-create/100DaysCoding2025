4b8b55edb5d0c2663c1e143a1f22c54503e9916d
Trihindari028-create <nndar028@gmail.com>
2025-09-26 20:13:40 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+	        double nilaiDouble = 123.456;
+	        int nilaiInt = (int) nilaiDouble;  // Memotong desimal, hasil 123 
+	        System.out.println("Double: " + nilaiDouble + ", setelah ke int: " + nilaiInt);
+	}
+}
