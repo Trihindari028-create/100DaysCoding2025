5bab045c9c630673b309f5b6e622d64a32ea2883
Trihindari028-create <nndar028@gmail.com>
2025-10-02 20:26:04 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+	 int i = 5; //Nilai awal 5
+	    System.out.println("awal: " + i);
+	    System.out.println("\nNilai Pre-increment: ");
+	    System.out.println("++i: " + ++i + "(i sekarang: " + i + ")");
+	  
+	    System.out.println("i++: " + i++ + "(i sekarang: " + i + ")");
+	  
+	    System.out.println("\nNilai Pre-decrement");
+	    System.out.println("--i: " + --i + "(i sekarang: " + i + ")");
+	  
+	    System.out.println("i--: " + i-- + "(i sekarang: " + i + ")");
+	}
+}
