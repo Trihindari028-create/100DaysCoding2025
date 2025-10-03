fe1deb0b0890d007a5c11d3c40313f5f8b5696c1
Trihindari028-create <nndar028@gmail.com>
2025-10-03 20:41:47 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,22 @@
+public class Main {
+ 
+	public static void main(String[] args) {
+int angka = 22;
+	    System.out.println("Nilai Awal: " + angka);
+	    System.out.println("----------------");
+	    
+	    angka = 12;
+	    System.out.println("Setelah angka = 12: " + angka);
+	    angka += 15;
+	    System.out.println("Setelah angka += 15: " + angka);
+	    angka -= 14;
+	    System.out.println("Setelah angka -= 14: " + angka);
+	    angka *= 8;
+	    System.out.println("Setelah angka *= 8: " + angka);
+	    angka /= 4;
+	    System.out.println("Setelah angka /= 4: " + angka);
+	    angka %= 3;
+	    System.out.println("Setelah angka %= 3: " + angka);
+
+}
+}
