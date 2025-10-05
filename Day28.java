76c27bf76c8454367a46a259b6a1aff4f46929d4
Trihindari028-create <nndar028@gmail.com>
2025-10-05 20:47:21 +0800

    Day28.java

--- Day28.java
+++ Day28.java
@@ -1,14 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
-	      int i = 5;
+	    int i = 5;
         int f = 5;
         int t = 10;
 
         // Cek sama (==)
-        System.out.println("x == y: " + (x == y));  // true, karena 5 sama 5
-        System.out.println("x == z: " + (x == z));  // false, karena 5 beda 10
+        System.out.println("i == f: " + (i == f));  // true, karena 5 sama 5
+        System.out.println("i == t: " + (i == t));  // false, karena 5 beda 10
         // Cek beda (!=)
-        System.out.println("x != y: " + (x != y));  // false, karena sama
-        System.out.println("x != z: " + (x != z));  // true, karena beda
+        System.out.println("i != f: " + (i != f));  // false, karena sama
+        System.out.println("i != t: " + (i != t));  // true, karena beda
     }
 }
