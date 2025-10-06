ea51d00af5c39da8cd1b02b412b9872258cfaf81
Trihindari028-create <nndar028@gmail.com>
2025-10-06 20:01:06 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,11 @@
+        int angka1 = 5;
+        int angka2 = 10;
+        
+        // Menggunakan operator <
+        boolean hasilKurangDari = angka1 < angka2;
+        System.out.println("Apakah " + angka1 + " < " + angka2 + "? " + hasilKurangDari);  // Output: true
+        
+        // Menggunakan operator >
+        boolean hasilLebihDari = angka1 > angka2;
+        System.out.println("Apakah " + angka1 + " > " + angka2 + "? " + hasilLebihDari);  // Output: false
+        
