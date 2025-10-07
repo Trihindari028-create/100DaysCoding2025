24dedc3da01af10b7d782e3ca052674960ff7e97
Trihindari028-create <nndar028@gmail.com>
2025-10-07 20:56:20 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+	      int angka1 = 5;
+        int angka2 = 10;
+        boolean hasilKurangsama = angka1 <= angka2;
+        System.out.println("Apakah " + angka1 + " <= " + angka2 + "? " + hasilKurangsama);  // Output: true
+        boolean hasilLebihsama = angka1 >= angka2;
+        System.out.println("Apakah " + angka1 + " >= " + angka2 + "? " + hasilLebihsama);  // Output: false
+    }
+}
