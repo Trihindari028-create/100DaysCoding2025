4eb111e4594ff746092fc3e503f86fce50931a84
Trihindari028-create <nndar028@gmail.com>
2025-10-09 21:26:28 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+	      boolean cuacapanas = true;  // Misal: hari ini cuca panas
+        boolean cuacahujan = false;  // Misal: hari ini cuaca hujan
+        
+        // Menggunakan operator OR (||) dalam kondisi if
+        if (cuacapanas || cuacahujan) {
+            System.out.println("beloh minum boba.");
+        } else {
+            System.out.println("tidak boloh minum boba.");
+        }
+}
+}
