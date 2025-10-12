fa391bc12e5b4cd81cc7f95841fdf6ba1bfc05a1
Trihindari028-create <nndar028@gmail.com>
2025-10-12 22:14:11 +0800

    Day35.java

--- Day35.java
+++ Day35.java
@@ -3,14 +3,10 @@ public class Main {
 	public static void main(String[] args) {
 	    Scanner indar = new Scanner(System.in);
 	            
-	            System.out.print("Masukkan sebuah bilangan bulat: ");
+	    System.out.print("Masukkan sebuah bilangan bulat: ");
         int bilangan = indar.nextInt();
         if (bilangan > 0) {
             System.out.println("Bilangan tersebut adalah POSITIF.");
-        } else if (bilangan < 0) {
-           System.out.print("Masukkan sebuah bilangan negatif: ");
-        } else {
-            System.out.println("Bilangan tersebut adalah NOL.");
-        }
+		}
 }
 }
