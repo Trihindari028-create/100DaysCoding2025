4dbc7fe87d5517129404881ff64561dc26ca5d8d
Trihindari028-create <nndar028@gmail.com>
2025-11-03 21:14:53 +0800

    Day57.java

---
+++ Day57.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+	for (int i = 1; i <= 5; i++) {
+		for (int f = 1; f <= i; f++ ) {
+			System.out.println("*");
+		}
+		System.out.println();
+	}
+ }
+}
