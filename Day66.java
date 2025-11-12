6b5e787e1bb36f1d76f8535633e5da333a9680ab
Trihindari028-create <nndar028@gmail.com>
2025-11-12 21:58:22 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		int tinggi = 5; 
+      
+        for (int i = 1; i <= tinggi; i++) {
+        
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+	}
+}
