e479ee5d68c9b34986e595c2303e8484db60bcf8
Trihindari028-create <nndar028@gmail.com>
2025-11-16 19:47:44 +0800

    Day70.java

---
+++ Day70.java
@@ -0,0 +1,12 @@
+public class day70 {
+    public static void main(String[] args) {
+        int lebar = 7;
+        int tinggi = 4;
+        for (int i = 0; i < tinggi; i++){
+            for (int r = 0; r < lebar; r++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
