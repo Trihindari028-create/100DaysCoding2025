03f355431f034c4b715d4e095e6982c36449307e
Trihindari028-create <nndar028@gmail.com>
2025-11-17 20:34:54 +0800

    Day71.java

---
+++ Day71.java
@@ -0,0 +1,11 @@
+public class day71 {
+    public static void main(String[] args) {
+        int tinggi = 5;
+        for (int i = 1; i <= tinggi; i++){
+            for (int r = 1; r <= i; r++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
