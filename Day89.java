ecfa9fca4ce8d2cbc7cd3b819e5fc35edb172383
Trihindari028-create <nndar028@gmail.com>
2025-12-05 21:34:20 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,20 @@
+public class day89 {
+    
+    public static void isihati(){
+        System.out.println("LAGI CAPEK LAGI GA BAIK-BAIK AJA.");
+    }
+    
+    public static void tampilkanNama(String nama) {
+        System.out.println("NAMA KAMU ADALAH: " + nama);
+    }
+    
+    public static void jumblah(int a, int b){
+        int hasil  = a + b;
+        System.out.println("HASIL PENJUMBLAHAN: " + hasil);
+    }
+    public static void main(String[] args) {
+        isihati();
+        tampilkanNama("INDAR");
+        jumblah(6,12);
+    }
+}
