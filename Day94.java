c3d49abe0fd65b222aecba8e597f6a47d9d46539
Trihindari028-create <nndar028@gmail.com>
2025-12-10 22:28:30 +0800

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,14 @@
+public class day94 {
+    
+    public static void cetakisihati(){
+        System.out.println("MAUU PULANG PLISS CAPEKK ");
+    }
+    
+    public static void cetaknama(String nama){
+        System.out.println("Nama Aku: " + nama);
+    }
+    public static void main(String[] args) {
+        cetakisihati();
+        cetaknama("Si_kecil");
+    }
+}
