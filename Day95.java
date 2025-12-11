4db123293c92443395c41e96b5e85478c82042a4
Trihindari028-create <nndar028@gmail.com>
2025-12-11 21:31:35 +0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,9 @@
+public class day95 {
+    
+    static void cetak(String nama, int umur){
+        System.out.println("HAY " + nama + "umur kamu " + umur + " Tahun ");
+    }
+    public static void main(String[] args) {
+        cetak("indar ", 19);
+    }
+}
